1d0f8da4e1652c0fe6777d218a23a2336597e488
Kerim Imamovic <79793398+kimamovic21@users.noreply.github.com>
2022-05-06 14:14:12 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { data } from "./mockData";
+import React from "react";
+import {MdChevronLeft, MdChevronRight} from 'react-icons/md';
+
+function App() {
+
+  const slideLeft = () => {
+    var slider = document.getElementById('slider')
+    slider.scrollLeft = slider.scrollLeft - 500;
+  }
+
+  const slideRight = () => {
+    var slider = document.getElementById('slider')
+    slider.scrollLeft = slider.scrollLeft + 500;
+  }
+
+  return (
+    <>
+    <img className="w-full h-[440px] object-cover"
+         src="https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2607&q=80" 
+         alt="Do something great" 
+    />
+    <div className="relative flex items-center">
+      <MdChevronLeft className="opacity-50 cursor-pointer hover:opacity-100" 
+                     onClick={slideLeft} 
+                     size={40}
+      />
+      <div id="slider"
+           className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide">
+         {data.map((item) => (
+           <img className="w-[220px] inline-block p-2 cursor-pointer
+                           hover:scale-105 ease-in-out duration-300"
+                src={item.img}
+                alt="/"/>
+         ))}
+      </div>
+      <MdChevronRight className="opacity-50 cursor-pointer hover:opacity-100"
+                      onClick={slideRight} 
+                      size={40}
+      />
+    </div>
+    </>
+  );
+}
+
+export default App;
